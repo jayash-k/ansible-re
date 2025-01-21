ad13176995996524588697660f80ade9ba9067a0
Jayash Khadtare <143110118+jayash-k@users.noreply.github.com>
2025-01-21 17:30:18 +0530

    Update and rename nginx.yml to app.js

---
+++ app.js
@@ -0,0 +1,73 @@
+require("dotenv").config();
+const fs = require('fs');
+const https = require('https');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require('mongoose');
+const passport = require("./config/googleAuth");
+const bodyParser = require('body-parser');
+const session = require("express-session");
+
+const { PORT, MONGO_URI, JWT_SECRET, FRONT_END_URL } = process.env;
+
+const app = express();
+
+// SSL options
+const sslOptions = {
+    key: fs.readFileSync('/etc/letsencrypt/live/api.milestono.com/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/api.milestono.com/fullchain.pem'),
+};
+
+// Middleware
+app.use(cors({
+    origin: FRONT_END_URL,
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true
+}));
+app.use(express.json());
+app.use(session({
+    secret: JWT_SECRET,
+    resave: false,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// MongoDB connection
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    connectTimeoutMS: 30000,  // Increase the connection timeout
+    socketTimeoutMS: 30000    // Increase the socket timeout
+})
+.then(() => {
+    console.log('MongoDB connected');
+
+    // Start the HTTPS server only after the connection is established
+    https.createServer(sslOptions, app).listen(PORT, () => {
+        console.log(`Server started on https://api.milestono.com:${PORT}`);
+    });
+})
+.catch(err => {
+    console.error(`MongoDB connection error: ${err.message}`);
+    process.exit(1);  // Exit the process with a failure
+});
+
+// Route handlers
+app.use('/api', require('./routes/userRoutes'));
+app.use('/auth', require('./routes/authRoutes'));
+app.use('/api', require('./routes/propertyRoutes'));
+app.use('/api', require('./routes/serviceRoutes'));
+app.use('/api', require('./routes/accountRoutes'));
+app.use('/api', require('./routes/paymentRoutes'));
+app.use('/api', require('./routes/otherRoutes'));
+app.use('/api', require('./routes/homePageRoutes'));
+app.use('/api', require('./routes/enquiryRoutes'));
+app.use('/api', require('./routes/feedbackRoutes'));
+app.use('/api', require('./routes/projectRoutes'));
+app.use('/api', require('./routes/galleryImageRoutes'));
+app.use('/api', require('./routes/bankRoutes'));
+
+module.exports = app;
